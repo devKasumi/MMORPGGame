1040952e62a09192c0e2f37d22c0c5d1a4333b0d
kasumi <dangnhabui@gmail.com>
2025-08-03 10:21:27 +0700

    Warrior Hero Anim Instance Create

---
+++ Source/MMORPG/Public/AnimInstances/BaseAnimInstance.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BaseAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMORPG_API UBaseAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+
+};
